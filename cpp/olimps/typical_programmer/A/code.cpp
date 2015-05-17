bcf24a35fc1448c1ad0bc8dcc238437100bf6951
Vadim Kiselev <vadim8kiselev@gmail.com>
2015-05-18 01:38:09 +0300

    Create code.cpp

---
+++ cpp/olimps/typical_programmer/A/code.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{		
+	string x;
+	cin >> x;
+
+	cout << "1 1 " + x;	
+	return 0;
+}
