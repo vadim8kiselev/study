3ef9ff21260886bf237fd0399c54eea91b0d1618
Vadim Kiselev <vadim8kiselev@gmail.com>
2015-06-29 13:45:17 +0300

    added first task

---
+++ cpp/practice/FirstTask/stack.cpp
@@ -0,0 +1,27 @@
+#include <fstream>
+#include <stack>
+#include <string>
+ 
+using namespace std;
+ 
+string base = "!;?.,-:;)(\"'";
+ 
+int main(){	
+	stack<char> st;	
+	ifstream in("input.txt");
+		string tmp;
+		while (in.peek() != EOF){
+			getline (in,tmp);
+			for (int index = 0; index < (int)tmp.length(); index++)
+				if (!(base.find(tmp[index]) == string::npos))
+					stack.push(tmp[index]);			
+		}
+	in.close();
+	
+	ofstream out("output.txt");
+		while (!st.empty()){
+			out << st.top();
+			st.pop();
+		}
+	out.close();
+}
