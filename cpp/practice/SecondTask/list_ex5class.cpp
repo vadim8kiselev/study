46294fd4728aaa69294cdc26ed437e65e615c9a3
Vadim Kiselev <vadim8kiselev@gmail.com>
2015-06-30 11:50:50 +0300

    Create list_ex5class.cpp

---
+++ cpp/practice/SecondTask/list_ex5class.cpp
@@ -0,0 +1,63 @@
+#include <fstream>
+#include <list>
+#include <string>
+using namespace std;
+
+class Games{
+
+	string type;
+	string name;
+
+public:
+	
+	Games(string a, string b) : type(a), name(b){}
+
+	string getType(){
+		return type;
+	}
+
+	string getName(){
+		return name;
+	}
+
+	void setQuest(){
+		name = "?" + name;
+	}
+
+	void print(ofstream &out){
+		out << type << " " << name << endl;
+
+	}
+
+};
+
+
+int main(){
+
+	list <Games> l;
+
+	ifstream in("input.txt");
+		string type, name;
+		while (in.peek() != EOF){
+			in >> type>> name;
+			Games tmp(type, name);
+			l.push_back(tmp);
+		}
+	in.close(); 
+		
+	for (list<Games>::iterator it = l.begin(); it != l.end();){
+		if (it->getType() == "Shooter")
+			it = l.erase(it);
+		else
+			it++;		
+	}
+	l.push_back(l.front());
+	l.pop_front();
+	
+	
+	ofstream out("output.txt");
+		for (auto i : l)
+			i.print(out);
+	out.close();
+	
+}
