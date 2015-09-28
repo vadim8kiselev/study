3c04b1d4ed0c673c96660614b4e3bfeab8b51602
Vadim Kiselev <vadim8kiselev@users.noreply.github.com>
2015-09-28 19:37:38 +0300

    Update n_order_max.cs

--- cs/tasks/n_order_max.cs
+++ cs/tasks/n_order_max.cs
@@ -8,32 +8,31 @@ namespace Solution
         {
             Console.Write("Enter size array: ");
             int size = int.Parse(Console.ReadLine());
+            
             Console.Write("Enter order of maximum: ");
-            int numberMax = int.Parse(Console.ReadLine());
+            int order = int.Parse(Console.ReadLine());
+            
             int[] array = new int[size];
+            
             int pointer = 0;
  
             foreach (string item in Console.ReadLine().Trim().Split(' '))
-            {
                 array[pointer++] = int.Parse(item);
-            }
  
             int max = int.MaxValue;
  
-            for (int index = 0; index < numberMax; index++)
+            for (int index = 0; index < order; index++)
             {
                 int currentMax = int.MinValue;
  
                 for (int jndex = 0; jndex < size; jndex++)
-                {
                     if (array[jndex] > currentMax && array[jndex] < max)
                         currentMax = array[jndex];
-                }
  
-                max = (currentMax != int.MinValue)?currentMax:max;
+                max = (currentMax != int.MinValue) ? currentMax : max;
             }
  
-            Console.WriteLine("Maximum of {0} order is {1}", numberMax, max);
+            Console.WriteLine("Maximum of {0} order is {1}", order, max);
         }
     }
 }
