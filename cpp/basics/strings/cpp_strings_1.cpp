abe9e02bfc015075b90478a73812068510dadb6e
Vadim Kiselev <vadim8kiselev@users.noreply.github.com>
2015-09-28 15:46:46 +0300

    Update cpp_strings_1.cpp

--- cpp/basics/strings/cpp_strings_1.cpp
+++ cpp/basics/strings/cpp_strings_1.cpp
@@ -7,14 +7,15 @@ int main()
 {
         ifstream in("input.txt");
         string str, str1;
-        getline(in, str); in>>str1;
+        getline(in, str); 
+        in>>str1;
  
         while (true)
         {
                 if (!(str.find(str1) == string::npos))
                         str.erase(str.find(str1), str1.length());
                 else break;            
-}
+        }
         ofstream out("output.txt");
                 out << str;
         out.close();
