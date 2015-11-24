fcfb2963641f687ff7150ac16eb48c67cd490cd1
Vadim Kiselev <vadim8kiselev@users.noreply.github.com>
2015-11-24 20:48:59 +0300

    Update lab2_ex2.asm

--- assembler/lab2_ex2.asm
+++ assembler/lab2_ex2.asm
@@ -2,48 +2,52 @@ stak segment stack 'stack'
   db 256 dup (?)
 stak ends
 data segment 'data'
-	value1 db 2h
-	value2 db 8h
+	value1 dw 2h
+	value2 dw 8h
 	next db '', 0Ah, 0Dh, '$'
 data ends
 code segment 'code'
 assume CS:code,DS:data,SS:stak
 
-print proc
-		push ax
-		mov ah,02h
-		int 21h
-		pop ax
-		ret
-print endp
+printDigit proc
+	push ax
+	mov ah,02h
+	int 21h
+	pop ax
+	ret
+printDigit endp
+
+printRow proc
+	pop bp
+	pop dx
+	add dl, 30h
+	call printDigit
+	mov dl, 20h
+	call printDigit
+	pop dx
+	add dl, 30h
+	call printDigit
+	push bp
+	ret
+printRow endp
 
 start:                         
-	mov AX,data
-	mov DS,AX
-	mov al, value1
-	mov dl,al
-	add dl,30h
-	call print
-	mov dl,20h
-	call print
-	mov bl, value2
-	mov dl,bl
-	add dl,30h
-	call print
+	mov ax, data
+	mov ds, ax
+	mov ax, 0
+	mov dx, 0
+	push value2 
+	push value1	
+	call printRow
 
 	mov ah,09h
 	lea dx, next
-	int 21h
-	xchg al,bl
+	int 21h	
+         
+	push value1
+	push value2
+	call printRow
 
-	mov dl,al
-	add dl,30h
-	call print
-	mov dl,20h
-	call print
-	mov dl,bl
-	add dl,30h
-	call print
 	mov AX,4C00h
 	int 21h
 code ends
