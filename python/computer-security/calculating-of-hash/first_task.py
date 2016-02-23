9bab8ccd052f84fd919dd303f04b64a8f1bc3b77
vadim8kiselev <vadim8kiselev@gmail.com>
2016-02-23 23:14:53 +0300

    Init data for first task of CS

--- python/computer-security/calculating-of-hash/first_task.py
+++ python/computer-security/calculating-of-hash/first_task.py
@@ -3,13 +3,13 @@ path = raw_input('Enter the path to file: ')
 
 hash_sum = 0
 
-with open(path) as f:
+with open('data/' + path, 'r') as f:
     while True:
         byte = f.read(1)
-        if not byte:
-            break
+        if byte:
+           hash_sum += ord(byte)
         else:
-            hash_sum += ord(byte)
+            break
 
 hash_sum %= 2**16
 
